6e9c66ee23578d2611b0f1e893d476041f48a8a3
May <22424005@student.hcmus.edu.vn>
2025-12-26 21:18:36 +0700

    update ss09

---
+++ SS07/index.js
@@ -0,0 +1,28 @@
+// let tong = 0;
+// for( let i = 1; i <= 100; i++ ) {
+//     tong = tong + i;
+// }
+// console.log("Tổng từ 1 đến 100 là: " + tong);
+
+
+// for( let i = 0; i<= 100; i++){
+//     if( i % 3 === 0 && i % 5 === 0 ){
+//         console.log(i);
+//     }
+// }
+
+// let n = 2;
+// let giaiThua = 1;
+// for( let i = 1; i <= n; i++ ){
+//     giaiThua = giaiThua * i;
+// }
+// console.log("Giai thừa của " + n + " là: " + giaiThua);
+
+for (let i = 2; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + " x " + j + " = " + (i * j));
+    }
+}
+
+
+
